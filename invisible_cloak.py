8c7112f3bb53d9ee59420066456ac5b23c65ff3f
jeet404 <jeet63240@gmail.com>
2023-01-02 10:14:14 +0530

    Reformet Code.

--- invisible_cloak.py
+++ invisible_cloak.py
@@ -2,7 +2,9 @@ import cv2
 import numpy as np
 import time
 
-print("Hello Guys,This My Invisible Cloak Python Program.\nYou should ready for get Invisible\nKeep Red thing in your hand. ")
+print(
+    "Hello Guys,This My Invisible Cloak Python Program.\nYou should ready for get Invisible\nKeep Red thing in your "
+    "hand. ")
 
 video = cv2.VideoCapture(0)
 time.sleep(3)
@@ -10,31 +12,31 @@ time.sleep(3)
 background = 0
 
 for i in range(30):
-    ret,background=video.read()
+    ret, background = video.read()
 
-background = np.flip(background,axis=1)
+background = np.flip(background, axis=1)
 
 while True:
-    ret,img = video.read()
+    ret, img = video.read()
     img = np.flip(img, axis=1)
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    blur = cv2.GaussianBlur(hsv ,(35,35) , 0)
+    blur = cv2.GaussianBlur(hsv, (35, 35), 0)
 
-    lower = np.array([0,120,70])
-    upper = np.array([10,255,255])
-    mask01 = cv2.inRange(hsv , lower, upper)
+    lower = np.array([0, 120, 70])
+    upper = np.array([10, 255, 255])
+    mask01 = cv2.inRange(hsv, lower, upper)
 
-    lower_red = np.array([170,120,70])
-    upper_red = np.array([180,255,255]) 
+    lower_red = np.array([170, 120, 70])
+    upper_red = np.array([180, 255, 255])
     mask02 = cv2.inRange(hsv, lower_red, upper_red)
 
-    mask = mask01+mask02
+    mask = mask01 + mask02
 
-    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((5,5), np.uint8))
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((5, 5), np.uint8))
 
-    img[np.where(mask==255)] = background[np.where(mask==255)]
+    img[np.where(mask == 255)] = background[np.where(mask == 255)]
 
-    cv2.imshow("Display",img)
+    cv2.imshow("Display", img)
     cv2.waitKey(10)
-  
-#Author:-Jap
+
+# Author:-Jap
